b5c18e4ccc5563faf5746e322d225b0bf79d058d
wirecobweb <wirecobweb@gmail.com>
2020-02-20 21:36:18 +0100

    after prettier

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,108 +1,89 @@
 class fizzBuzz {
-    constructor(view) {
-
-      this.view = view;
-      this.sectionForm = this.view.getElementById("form-section");
-      this.form = this.view.createElement("form");
-      this.workersInput = this.view.createElement("input");
-      this.workersInput.placeholder = "workers";
-      this.numberInput = this.view.createElement("input");
-      this.numberInput.placeholder = "num";
-      this.submitButton = this.view.createElement("button");
-      this.submitButton.textContent = "Start";
-      this.form.append(this.workersInput, this.numberInput, this.submitButton);
-      this.sectionForm.append(this.form);
-      this.getValues();
-      this.arrayResponse
-      this.response= [];
-     
-    }
-  
-    get _workersInput() {
-      return this.workersInput.value;
-    }
-  
-    get _numberInput() {
-      return this.numberInput.value;
-    }
-  
-    resetForm() {
-      this.numberInput.value = "";
-      this.workersInput.value = "";
-    }
-
-  
-    getValues() {
-      this.form.addEventListener("submit", event => {
-        event.preventDefault();
-  
-        this.builderWorker(this._numberInput, this._workersInput);
-      });
-    }
-
-
-    pipe = (...functions) => initialValue =>
-      functions.reduce((value, funct) => funct(value), initialValue);
-
-
-    builderWorker(number, workers) {
-
-      const cuts = number / workers;
-  
-      const arrayNumber = [...Array(Number(number)).keys()];
-  
-      const workerGroup = [...Array(Number(workers))].map(
-        this.pipe(
-          this.createWorker,
-          this.asingAddEventListener
-        )
-      );
-  
-      const chunkArray = this.chunk(arrayNumber, cuts);
-  
-      chunkArray.map((job, index) => workerGroup[index].postMessage({index, job}));
-
-      
-    }
-  
-    recieved =(e)=> {
-      
-      const {index, answer} = e.data
-      this.response[index] = answer;
-      e.target.terminate();
-
-      this.display();
-     
-        
-    }
-
-   
-
-    display=()=>{
-      
-      console.log(this.response)
-    }
-
-  
-    createWorker() {
-      return new Worker("worker.js");
-    }
-  
-    asingAddEventListener = worker => {
-      worker.addEventListener("message", this.recieved);
-      return worker;
-    };
-  
-    chunk(array, size) {
-      if (!array.length) {
-        return [];
-      }
-      const head = array.slice(0, size);
-      const tail = array.slice(size);
-  
-      return [head, ...this.chunk(tail, size)];
-    }
-
-
-    
+  constructor(view) {
+    this.view = view;
+    this.sectionForm = this.view.getElementById("form-section");
+    this.form = this.view.createElement("form");
+    this.workersInput = this.view.createElement("input");
+    this.workersInput.placeholder = "workers";
+    this.numberInput = this.view.createElement("input");
+    this.numberInput.placeholder = "num";
+    this.submitButton = this.view.createElement("button");
+    this.submitButton.textContent = "Start";
+    this.form.append(this.workersInput, this.numberInput, this.submitButton);
+    this.sectionForm.append(this.form);
+    this.getValues();
+    this.arrayResponse;
+    this.response = [];
   }
+
+  get _workersInput() {
+    return this.workersInput.value;
+  }
+
+  get _numberInput() {
+    return this.numberInput.value;
+  }
+
+  resetForm() {
+    this.numberInput.value = "";
+    this.workersInput.value = "";
+  }
+
+  getValues() {
+    this.form.addEventListener("submit", event => {
+      event.preventDefault();
+
+      this.builderWorker(this._numberInput, this._workersInput);
+    });
+  }
+
+  pipe = (...functions) => initialValue =>
+    functions.reduce((value, funct) => funct(value), initialValue);
+
+  builderWorker(number, workers) {
+    const cuts = number / workers;
+
+    const arrayNumber = [...Array(Number(number)).keys()];
+
+    const workerGroup = [...Array(Number(workers))].map(
+      this.pipe(this.createWorker, this.asingAddEventListener)
+    );
+
+    const chunkArray = this.chunk(arrayNumber, cuts);
+
+    chunkArray.map((job, index) =>
+      workerGroup[index].postMessage({ index, job })
+    );
+  }
+
+  recieved = e => {
+    const { index, answer } = e.data;
+    this.response[index] = answer;
+    e.target.terminate();
+
+    this.display();
+  };
+
+  display = () => {
+    console.log(this.response);
+  };
+
+  createWorker() {
+    return new Worker("worker.js");
+  }
+
+  asingAddEventListener = worker => {
+    worker.addEventListener("message", this.recieved);
+    return worker;
+  };
+
+  chunk(array, size) {
+    if (!array.length) {
+      return [];
+    }
+    const head = array.slice(0, size);
+    const tail = array.slice(size);
+
+    return [head, ...this.chunk(tail, size)];
+  }
+}
--- worker.js
+++ worker.js
@@ -1,23 +1,16 @@
 addEventListener("message", received);
 
-function received(event){
+function received(event) {
+  const { index, job } = event.data;
 
-    const {index, job} = event.data
-   
+  const fizz = num => (num % 3 === 0 ? "Fizz" : num);
+  const buzz = num => (num % 5 === 0 ? "Buzz" : num);
+  const fizzBuzz = num => (num % 15 === 0 ? "FizzBuzz" : num);
 
+  const compose = (...functions) => initialValue =>
+    functions.reduceRight((value, funct) => funct(value), initialValue);
+  const composeFizzBuzz = compose(fizz, buzz, fizzBuzz);
+  const answer = job.map(composeFizzBuzz);
 
-    const fizz = (num)=> (num % 3 === 0) ? "Fizz" : num;
-    const buzz = (num)=> (num % 5 === 0) ? "Buzz" : num;
-    const fizzBuzz = (num)=> (num % 15 === 0) ? "FizzBuzz" : num;
-
-
-    const compose = (...functions)=> initialValue => functions.reduceRight((value, funct) => funct(value), initialValue);
-    const composeFizzBuzz = compose(fizz,buzz,fizzBuzz);
-    const answer = job.map(composeFizzBuzz);
-
- 
-    postMessage({index, answer});
-
-
-
+  postMessage({ index, answer });
 }
